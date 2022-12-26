465eae472bdc8a65b4a04e8846fce27abbad607a
wooseohyeon <smtas257@gmail.com>
2022-12-26 14:10:58 +0900

    test

--- 007.cpp
+++ 007.cpp
@@ -6,8 +6,23 @@ using namespace std;
 
 int main()
 {
-	cout << "hello world" << endl;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	float x1 = 0;
+	float x2 = 0;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	x1 = (-b + (float)sqrt(b * b - 4 * a * b)) / (2 * a);
+	x2 = (-b - (float)sqrt(b * b - 4 * a * b)) / (2 * a);
+
+
+	cout << x1 << endl;
+	cout << x2 << endl;
 
 	return 0;
-
-	}
+}
