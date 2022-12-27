0831d2dd224736248cb5b9022414b12b9645de0a
wooseohyeon <smtas257@gmail.com>
2022-12-27 17:45:08 +0900

    test

--- 007.cpp
+++ 007.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-using namespace std;
+//#include <iostream>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <math.h>
+//using namespace std;
 
 //int main()
 //{
