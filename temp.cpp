07987a6234dec831364c2ed7f0a5d9a279872ec1
wooseohyeon <smtas257@gmail.com>
2023-01-05 17:55:26 +0900

    test

---
+++ temp.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int asdf()
+//{
+//	int result = 0;
+//	return result;
+//}
+//
+//int test(int num1, int num2) // int num1 = 2, int num2 = 3
+//{
+//	int result2 = 0;
+//
+//	cout << num1 << num2;
+//
+//	return result2;
+//}
+//
+//int main()
+//{
+//	int a;
+//	a = asdf();
+//
+//	test(2,3);
+//
+//
+//
+//	return 0;
+//}
