6bc5a7d95d50313fc054e1130e073d450a7142c6
wooseohyeon <smtas257@gmail.com>
2023-01-06 17:56:13 +0900

    text

--- 014.cpp
+++ 014.cpp
@@ -1,26 +1,20 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-using namespace std;
-
-class BingGo
-{
- public:
-
-	 int A = 10;
-	 int B = 20;
-	 int C = 15;
-	 int D = 17;
-	 int E = 30;
-	 
-	BingGo(int a)
-	{
-
-		  A = a;
-
-	}
-		
-	BingGo 
-
-};
+//#include <iostream>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <math.h>
+//using namespace std;
+//
+//class BingGo
+//{
+//public:
+//
+//	BingGo()
+//	{
+//
+//
+//
+//	}
+//
+//
+//
+//};
